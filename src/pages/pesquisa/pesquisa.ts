709a296c4625f5a3078fa0b66d5683831960efc0
Murillo Falcao <murillo.falcao@gmail.com>
2018-07-04 08:59:09 -0300

    Modificacoes na page Pesquisa

--- src/pages/pesquisa/pesquisa.ts
+++ src/pages/pesquisa/pesquisa.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the PesquisaPage page.
@@ -15,11 +16,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PesquisaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PesquisaPage');
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'VEÍCULO INCLUIDO... OU NÃO',
+      duration: 2000,
+      position: 'top',
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
   }
 
 }
